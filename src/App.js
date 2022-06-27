9d6f780a0977d879499abb6d56b71d461e92104b
GrahamA4 <abraham3105schl@gmail.com>
2022-06-27 16:04:00 -0400

    Basic Layout Done and Api Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,316 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import styles from './App.module.css';
+import cs from 'classnames';
+import styled from 'styled-components';
+import { ReactComponent as Check } from './check.svg';
+
+///////*********************************/////////
+///////********[ CUSTOM STATE ]*********////////
+///////********************************////////
+
+const useSemiPersistentState = (key, initialState) => {
+  const isMounted = React.useRef(false);
+
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+      localStorage.setItem(key, value);
+    }
+  }, [value, key]);
+
+  return [value, setValue];
+};
+//*****************************************************************//
+
+///////************************************************************//////
+///////[ SETS-UP STATE STRUCTURE FOR STORIES BASED ON USER ACTION ]/////
+///////**********************************************************//////
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return { ...state, isLoading: true, isError: false };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return { ...state, isLoading: false, isError: true };
+
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        )
+      };
+    default:
+      throw new Error();
+  }
+};
+
+//*****************************************************************//
+
+///////**************************************************//////////////
+///////*********[ ANGOLIA API FOR SEARCH LIST ]*********//////////////
+///////************************************************//////////////
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+//*****************************************************************//
+
+const getSumComments = (stories) => {
+  // console.log('C');
+
+  return stories.data.reduce((result, value) => result + value.num_comments, 0);
+};
 
 function App() {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false
+  });
+
+  const handleFetchStories = React.useCallback(async () => {
+    if (!searchTerm) return;
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    try {
+      const result = await axios.get(url).then((result) => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.data.hits
+        });
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+  }, [url]);
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
+
+  //Handles Remove Story action
+  const handleRemoveStory = React.useCallback((item) => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item
+    });
+  }, []);
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  //Handles Search Story action
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
+  };
+
+  const searchedStories = stories.data.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const sumComments = React.useMemo(() => getSumComments(stories), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
+      {stories.isError && <p>Something went wrong ...</p>}
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
+    </StyledContainer>
   );
 }
+
+///////*************************************************************////////
+//////*************[ REUSABLE SEARCH RESULT STRUCTURE ]*************////////
+//////*************************************************************///////
+
+const List = React.memo(({ list, onRemoveItem }) =>
+  // console.log('B:List') ||
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ))
+);
+const Item = ({ item, onRemoveItem }) => {
+  function handleRemoveItem() {
+    onRemoveItem(item);
+  }
+  return (
+    <StyledItem>
+      <StyledColumn width="40%">
+        <a href={item.url}>{item.title}</a>
+      </StyledColumn>
+      <StyledColumn width="30%">{item.author} </StyledColumn>
+      <StyledColumn width="10%">{item.num_comments} </StyledColumn>
+      <StyledColumn width="10%">{item.points}</StyledColumn>
+      <StyledColumn width="10%">
+        <StyledButtonSmall
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          className={`${styles.button} ${styles.buttonSmall}`}
+        >
+          <Check height="18px" width="18px" />
+        </StyledButtonSmall>
+      </StyledColumn>
+    </StyledItem>
+  );
+};
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <StyledSearchForm onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <StyledButtonLarge type="submit" disabled={!searchTerm}></StyledButtonLarge>
+  </StyledSearchForm>
+);
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+      &nbsp;
+      <StyledInput
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+        className={styles.input}
+      />
+    </>
+  );
+};
+//*****************************************************************//
+
+function getTheTruth() {
+  if (console.log('B:List')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(getTheTruth());
+//*****************************************************************//
+const StyledItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow:ellipsis a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+background: transparent;
+border: 1px solid #171212;
+padding: 5px;
+cursor: pointer;
+
+transition: all 0.1s ease-in
+
+&:hover {
+  background: #171212;
+  color: #ffffff;
+}`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+border: none;
+border-bottom: 1px solid #171212;
+background color: transparent;
+
+font-size: 24px;`;
+
+//********* **********/
 
 export default App;
